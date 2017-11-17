d2045aacd5df8973ad191dc802ebb9467db9bbae
AlbertKobiakov <sdf15SDS666BvZZ>
2017-11-18 00:11:47 +0300

    Added cardviews.

--- app/src/main/java/com/example/albert/remindme/fragment/HistoryFragment.java
+++ app/src/main/java/com/example/albert/remindme/fragment/HistoryFragment.java
@@ -3,15 +3,22 @@ package com.example.albert.remindme.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.albert.remindme.R;
+import com.example.albert.remindme.adapter.RemindListAdapter;
+import com.example.albert.remindme.dto.RemindDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryFragment extends AbstractTabFragment {
 
-    private static final int LAYOUT = R.layout.fragment_example;
+    private static final int LAYOUT = R.layout.fragment_history;
 
     public static HistoryFragment getInstance(Context context){
         Bundle args = new Bundle();
@@ -31,6 +38,24 @@ public class HistoryFragment extends AbstractTabFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
+
+        RecyclerView rv = view.findViewById(R.id.recyclerView);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new RemindListAdapter(createMockRemindListData()));
         return view;
+    }
+
+    private List<RemindDTO> createMockRemindListData() {
+
+        List<RemindDTO> data = new ArrayList<>();
+
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        data.add(new RemindDTO("Item 6"));
+
+        return data;
     }
 }
